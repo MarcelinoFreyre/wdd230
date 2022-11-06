e65423f7c007b0d7752c655ae05d72cb5de187c1
marcelinofreyre <marceeunion@gmail.com>
2022-11-06 00:17:56 -0300

    Added form and thank you pages

--- chamber/scripts/styles.js
+++ chamber/scripts/styles.js
@@ -4,6 +4,9 @@ const mainnav = document.querySelector('.navigation');
 
 const datefield = document.querySelector(".date");
 
+const join_date = document.querySelector(".date");
+
+let title = document.getElementById('title')
 /*Declare variable to hold */
 
 const now = new Date();
@@ -26,24 +29,6 @@ document.getElementById(
 	"currentday"
 ).textContent = `Last Modification: ${document.lastModified}`;
 
-// try {
-//     let options = {
-//         weekday: "long",
-//         day : "numeric",
-//         month: "long",
-//         year: "numeric",
-//     };
-//     document.getElementById("currentday").textContent = new Date().toLocaleDateString("en-US", options);
-// } catch(e) {
-//     alert("Error with code or your browser does not support Locale");
-// }
-
-// document.querySelector('#year').textContent = new Date().getFullYear();
-
-// -- -- -- -- -- -- -- -- -- -- -- -- -- -->
-
-// const day = now.getDate(); //--> 6   Necesitamos que sea igual  a 1 y 2
-
 const today = new Date().toLocaleDateString('en-us', { weekday:"long"});
 const message = document.createElement("h3");
 message.textContent='🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
@@ -52,3 +37,11 @@ if (today ==="Monday" || today ==="Tuesday"){
     message.style.backgroundColor = "white";
     divBanner.appendChild(message);
 }
+
+/*----------------------------------------------------------------------------------------------------------------------*/
+/*Week 8 */
+
+const date_time = now.toJSON();
+
+join_date.innerHTML = `<em>${date_time}</em>`;
+
