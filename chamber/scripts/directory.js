2c7edf843753a64b7a00ce335b09a47a52d88ce3
marcelinofreyre <marceeunion@gmail.com>
2022-11-13 02:54:55 -0300

    changed js file

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,25 +1,25 @@
 const requestURL = 'https://github.com/MarcelinoFreyre/wdd230/blob/main/chamber/json/companies.json';
-const cards = document.querySelector('.cards');
+const cards = document.querySelector('#cards');
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    // console.table(jsonObject);  // temporary checking for valid response and data parsing
     const companies = jsonObject['companies']
     companies.forEach(displayCompanies);
   });
-
 
 
 function displayCompanies(company) { 
   // Create elements to add to the document
   let card = document.createElement('section');
   let h2 = document.createElement('h2');
-  let portrait = document.createElement('img');
+  let logo = document.createElement('img');
   let p = document.createElement('p');
   let p2 = document.createElement('p');
+  let p3 = document.createElement('p');
 
 
   // Change the textContent property of the h2 element to contain the company name
@@ -27,20 +27,22 @@ function displayCompanies(company) {
 
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-  portrait.setAttribute('src', company.imageurl);
-  portrait.setAttribute('alt', 'Portait of ' + company.name);
-  portrait.setAttribute('loading', 'lazy');
+  logo.setAttribute('src', company.imageurl);
+  logo.setAttribute('alt', 'Logo of ' + company.name);
+  logo.setAttribute('loading', 'lazy');
 
   // Add/append the section(card) with the h2 element
   card.appendChild(h2);
-  card.appendChild(p)
-  card.appendChild(p2)
-  card.appendChild(portrait);
+  card.appendChild(p);
+  card.appendChild(p2);
+  card.appendChild(p3);
+  card.appendChild(logo);
 
   // Add birthdate data
   p.textContent = `Phone number: ${company.phone}`
   p2.textContent = `Location: ${company.address}`
+  p3.textContent = `Membership level: ${company.level}`
 
-  // Add/append the existing HTML div with the cards class with the section(card)
-  document.querySelector('div.cards').appendChild(card);
+  // Add/append the existing HTML div with the cards class with the id(card)
+  cards.appendChild(card);
 }
